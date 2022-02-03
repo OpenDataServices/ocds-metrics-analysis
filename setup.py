6818f97ddf5dd37f0e4f8530a7992535b2e39659
James <james.baster@opendataservices.coop>
2022-02-03 10:24:11 +0000

    release: 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ocdsmetricsanalysis",
-    version="0.0.0",
+    version="0.1.0",
     author="Open Data Services",
     author_email="code@opendataservices.coop",
     url="https://github.com/OpenDataServices/ocds-metrics-analysis",
